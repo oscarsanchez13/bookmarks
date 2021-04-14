67eb10e764beed114f18ee600738979c01111e70
architectsanchez13@gmail.com <you@example.com>
2021-04-13 20:02:34 -0500

    fixed bug(typo)

--- src/store.js
+++ src/store.js
@@ -1,22 +1,22 @@
-const uuid = require('uuid/v4')
+const { v4: uuidv4 } = require('uuid');
 
 const bookmarks = [
-  { id: uuid(),
+  { id: uuidv4(),
     title: 'Thinkful',
     url: 'https://www.thinkful.com',
     description: 'Think outside the classroom',
     rating: 5 },
-  { id: uuid(),
+  { id: uuidv4(),
     title: 'Google',
     url: 'https://www.google.com',
     description: 'Where we find everything else',
     rating: 4 },
-  { id: uuid(),
+  { id: uuidv4(),
     title: 'MDN',
     url: 'https://developer.mozilla.org',
     description: 'The only place to find web documentation',
     rating: 5 },
-  { id: uuid(),
+  { id: uuidv4(),
     title: 'LinkedIn',
     url: 'https://www.linkedin.com/in/oscarsanchez13/',
     description: 'Best place to find a reliable Full-stack Software Engineer',
